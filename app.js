6bf5de6de676e9597f36c0e7ba288826f2f65d17
Pratomo Ardianto <ardi@x-team.com>
2014-10-28 18:07:25 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+(function() {
+  var apiEndPoint = 'http://baker-bakes.dev/ngdrupal',
+  	  app = angular.module('drupalNg', ['ngResource', 'ngRoute']);
+
+  app.config(function($routeProvider, $locationProvider) {
+  	$routeProvider
+  	  .when('/', {
+  	    templateUrl: 'pages/home.html',
+  	    controller: 'HomepageImages'
+  	  })
+  	  .when('/about', {
+  	  	templateUrl: 'pages/about-us.html',
+  	    controller: 'AboutUsCtrl'
+  	  })
+  	  .when('/contact', {
+  	  	templateUrl: 'pages/contact-us.html',
+  	  	controller: 'ContactUsCtrl'
+  	  });
+
+  	  $locationProvider.html5Mode(true);
+  });
+
+  app.controller('HomepageImages', ['$scope', '$http', function($scope, $http) {
+  	var that = this;
+  	that.images = [];
+
+  	$http.get(apiEndPoint + '/homepage_images').
+        success(function(data) {
+        	that.images = data.homepage_images;
+        });
+  }]);
+
+  app.controller('AboutUsCtrl', ['$scope', '$http', function($scope, $http) {
+  	var that = this;
+  	that.recipes = [];
+
+  	$http.get(apiEndPoint + '/recipes').
+        success(function(data) {
+        	that.recipes = data.recipes_content;
+        });
+  }]);
+
+  app.controller('ContactUsCtrl', function($scope) {
+  	$scope.message = 'This is from Contact Us y\'all!';
+  });
+
+})();
+
+
+
+
+
